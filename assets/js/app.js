01c6dc9750ebc4a43323fa1357f85f08c3cbcf79
guilherme <guguiejo@gmail.com>
2023-09-03 12:15:45 -0300

    .

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+var MenuItens = document.getElementById("MenuItens");
+
+MenuItens.style.maxHeight = "0px";
+
+function menuCelular(){
+    if(MenuItens.style.maxHeight == "0px") {
+        MenuItens.style.maxHeight = "200px";
+    }else {
+        MenuItens.style.maxHeight = "0px";
+    }
+}
+
+
+var produtoImg = document.getElementById("produtoImg");
+var  produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
+
+produtoMiniatura[0].onclick = function() {
+    produtoImg.src = produtoMiniatura[0].src;
+}
+
+produtoMiniatura[1].onclick = function() {
+    produtoImg.src = produtoMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function() {
+    produtoImg.src = produtoMiniatura[2].src;
+}
+
+produtoMiniatura[3].onclick = function() {
+    produtoImg.src = produtoMiniatura[3].src;
+}
+
+
